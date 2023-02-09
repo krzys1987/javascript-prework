0b64e11e1a1c888f4fb169e7ff51f0aa494ff982
Krzysztof A. Kluza <krzysztof.a.kluza@gmail.com>
2023-02-09 16:26:44 +0100

    setup script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
-printMessage('Zagrałem + computerMove + Jeśli Twój ruch to + playerMove +, to wygrywasz!');
 let playerMove='papier';
 let computerMove ='kamien';
+printMessage('Zagrałem + computerMove + Jeśli Twój ruch to + playerMove +, to wygrywasz!');
+
