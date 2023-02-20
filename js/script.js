fe789c10ff64ee439537ba9339fcce29926a7185
Krzysztof A. Kluza <krzysztof.a.kluza@gmail.com>
2023-02-20 17:39:11 +0100

    noughts and crosses

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
-let playerMove='papier';
-let computerMove ='kamien';
-printMessage('Zagrałem + computerMove + Jeśli Twój ruch to + playerMove +, to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-/* randomNumber */
-let randomNumber = Math.floor(Math.random(11-19) * 3 + 1);
-console.log("random-number:", randomNumber);
-console.log("playerMove", playerMove);
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień'; }
+  else if(randomNumber == 2){
+    computerMove = 'papier';
+  } else if(randomNumber = 3 {
+    computerMove = 'nożyce' }
+
+printMessage('Mój ruch to: ' + computerMove);
