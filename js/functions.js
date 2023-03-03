5db5f9772be2540ef901418574998041f4574745
Krzysztof A. Kluza <krzysztof.a.kluza@gmail.com>
2023-03-03 17:06:20 +0100

    still working, it's ok but not right

--- js/functions.js
+++ js/functions.js
@@ -12,4 +12,31 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-
+function displayResult(computerMove, playerMove) {
+	/* printMessage("Komputer losuje" + computerMove, + "a ja" + playerMove ); */
+	console.log("Wylosowane ruchy to:" + computerMove + playerMove );
+		if (computerMove == playerMove) {
+			printMessage("Remis");
+		}
+		if (computerMove == "papier" && playerMove == "kamień") {
+			printMessage("Przegrałeś!");
+		}
+		if (computerMove == "papier" && playerMove == "nożyce") {
+			printMessage("Wygrałeś!");
+		}
+		if (computerMove == "kamień" && playerMove == "papier") {
+			printMessage("Wygrałeś!");
+		}
+		if (computerMove == "kamień" && playerMove == "nożyce") {
+			printMessage("Przegrałeś!");
+		}
+		if (computerMove == "nożyce" && playerMove == "papier") {
+			printMessage("Przegrałeś!");
+		}
+		if (computerMove == "nożyce" && playerMove == "kamień") {
+			printMessage("Wygrałeś!");
+		}
+		if (playerMove = "nieznanyRuch") {
+			printMessage("Losuj ponownie");
+		}
+	}
