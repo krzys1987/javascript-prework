beaad4ca6e899131fade503b3b064cc4cf30b4d6
Krzysztof A. Kluza <krzysztof.a.kluza@gmail.com>
2023-03-10 18:39:05 +0100

    name to english

--- js/script.js
+++ js/script.js
@@ -3,16 +3,17 @@ alert("Gra w kółko i krzyżyk");
 printMessage("Gra w kółko i krzyżyk");
 console.log("Gra w kółko i krzyżyk");
 
+
 //buttons and action
 function buttonClicked() {
   printMessage("Wybór został dokonany");
 }
-let zagrajKamien = document.getElementById("play-rock");
-zagrajKamien.addEventListener("click", buttonClicked);
-let zagrajPapier = document.getElementById("play-paper");
-zagrajPapier.addEventListener("click", buttonClicked);
-let zagrajNozyce = document.getElementById("play-scissors");
-zagrajKamien.addEventListener("click", buttonClicked);
+let playRock = document.getElementById("play-rock");
+playRock.addEventListener("click", buttonClicked);
+let playPaper = document.getElementById("play-paper");
+playPaper.addEventListener("click", buttonClicked);
+let playScissors = document.getElementById("play-scissors");
+playScissors.addEventListener("click", buttonClicked);
 
 document.getElementById("play-rock").addEventListener("click", function() {
   playGame(1);
@@ -26,21 +27,14 @@ document.getElementById("play-scissors").addEventListener("click", function() {
 
 //Counting-Victories
 
-//document.getElementById('game!').addEventListener(playGame(playerInput)), printMessage("Ilość wygranych", printMessage(result));
-
-//let div = document.createElement('div');
 /*
-
-document.getElementById("play-rock").addEventListener("click", function() {
-    printMessage("Zagrałem"+getMoveName)
-})
-document.getElementById("play-paper").addEventListener("click", function() {
-    printMessage("Zagrałem"+getMoveName)
-})
-document.getElementById("play-scissors").addEventListener("click", function() {
-    printMessage("Zagrałem"+getMoveName)
-})
-*/
+      let computerWins = displayResult(computerMove, playerMove)
+      let playerWins = displayResult(computerMove, playerMove) {
+      } else if (computerMove == "kamień" && playerMove == "papier") {
+      } else if (computerMove == "kamień" && playerMove == "nożyce") {
+      } else if (computerMove == "nożyce" && playerMove == "kamień") {
+      } else if (computerMove == "nożyce" && playerMove == "kamień") {
+  */
 
 //game
 function playGame(playerInput) {
@@ -88,16 +82,10 @@ function playGame(playerInput) {
       printMessage("Wygrałeś!");
     }
   }
-  //let computerWins = ();
-  //let playerWins();
+
   printMessage("Gra skończona, w jej rezultacie...");
   displayResult(computerMove, playerMove);
   console.log("Gra skończona" + displayResult(playerMove));
 }
 
-playGame(playerInput);
-
-/* napisac cos, aby result było
-function result(msg) {
-  printMessage("Dotyczas:");
-} */
+//playGame(playerInput);
